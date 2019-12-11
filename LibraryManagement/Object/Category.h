bacf4046c7eada1e28e24fa947d57b329a7123d2
nthanhvinh160 <52741143+nthanhvinh160@users.noreply.github.com>
2019-12-11 11:27:15 +0700

    Category

--- LibraryManagement/Object/Category.h
+++ LibraryManagement/Object/Category.h
@@ -12,7 +12,17 @@ class Category{
         string categoryName;
         string moreInfo;
 	public:
+		Category(int categoryID, string categoryName, string moreInfo);
 		friend ostream& operator << (ostream& out, const Category& category);
 };
-
+Category::Category(int categoryID, string categoryName, string moreInfo):
+	 categoryID(categoryID),categoryName(categoryName),moreInfo(moreInfo){}
+ostream% operator << (ostream& out, const Category& category){
+	out << "Category ID: " << category.categoryID << "\n";
+	out << "Category Name: " << category.categoryName << "\n";
+	out << "Category Info: " << category.categoryInfo << "\n";
+}
+ void addBook(Book* book){
+ 	book.Category;
+ }	
 //
