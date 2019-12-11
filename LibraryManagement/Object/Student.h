7fc32a53f303a707bd2b74f8e0ae6fbaf75a8702
nthanhvinh160 <52741143+nthanhvinh160@users.noreply.github.com>
2019-12-11 10:08:43 +0700

    hello
    world

--- LibraryManagement/Object/Student.h
+++ LibraryManagement/Object/Student.h
@@ -11,3 +11,19 @@ class Student{
         string address;
         int phoneNumber;
 };
+public:
+    Student(int userID, string name, string author, int categoryId, int numCopy);
+		friend ostream& operator << (ostream& out, const Book& book);
+};
+/*======================================*/
+
+Student::Student(int userID,string username, string address,int phoneNumber ):
+    userID(userID),username(username),address(address),phoneNumber(phoneNumber)
+    {}
+ostream& operator << (ostream& out, const Student& student){
+     out << "user ID: " << student.userID << "\n";
+     out << "User Name: " << student.username << "\n";
+     out << "Address: " << student.address << "\n";
+     out << "PhoneNumber: " << student.phoneNumber << "\n";
+         
+}
