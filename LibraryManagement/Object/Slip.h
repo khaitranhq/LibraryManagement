fb5a9d2f1ef5fdd9a6e67e25ba1050cd5c804662
nthanhvinh160 <52741143+nthanhvinh160@users.noreply.github.com>
2019-12-11 11:18:10 +0700

    Slip

--- LibraryManagement/Object/Slip.h
+++ LibraryManagement/Object/Slip.h
@@ -6,6 +6,9 @@
 class Date{
     private:
         int day, month, year;
+    public:
+    Date(int day, int month, int year);
+    friend ostream& operator << (ostream& dout, const Date& date);
 };
 
 class Slip{
@@ -15,4 +18,34 @@ class Slip{
         int userID;
         Date dateStart, dateEnd;
         bool status;
-};
+    public:
+    Slip(int slipID,int userID, Date dateStart, Date dateEnd, bool status);
+    friend ostream& operator << (ostream& out, const Slip& slip);
+    
+};    
+    /*================*/
+    
+    Slip::Slip(int slipID, int userID, Date dateStart, Date dateEnd, bool status):
+     slipID(slipID),books(books),userID(userID),dateStart(dateStart),dateEnd(dateEnd),status(status)
+     {}
+    Date::Date(int day,int month,int year):day(day),month(month),year(year){}
+ostream& operator << (ostream& out, const Slip& slip){
+     out << "slip ID: " << slip.slipID << "\n";
+     out << "User ID: " << slip.userID << "\n";
+     out << "Date Start: " << slip.dateStart << "\n";
+     out << "Date End: " << slip.dateEnd << "\n";
+     out << "Status: " << slip.status << "\n";    
+}
+ostream& operator << (ostream& dout, const Date& date){
+    dout << "Day: " << date.day << "\n";
+    dout << "Month: " << date.month << "\n";
+    dout << "Year : " << date.year << "\n";
+}
+void Book(Book* books) {
+    books.Slip;
+}
+        
+               
+        
+
+
