5a337e9751b6e8bb3092f2c91f4184530a3a7799
syphuongcuong3 <leoalan5577@gmail.com>
2019-12-15 12:12:48 +0700

    add Date.h

--- LibraryManagement/Container/Date.h
+++ LibraryManagement/Container/Date.h
@@ -1,1 +1,29 @@
 #pragma once
+
+#include <iostream>
+using namespace std;
+
+class Date
+{
+private:
+	int day, month, year;
+
+public:
+	Date(int day = 0, int month = 0, int year = 0);
+	Date(const Date& date);
+	friend ostream& operator<<(ostream& out, const Date& date);
+};
+
+/*=============Definition of Date============*/
+Date::Date(int day, int month, int year) :
+	day(day), month(month), year(year)
+{}
+
+Date::Date(const Date& date) :
+	day(date.day), month(date.month), year(date.year)
+{}
+
+ostream& operator << (ostream& out, const Date& date) {
+	out << date.day << "/" << date.month << "/" << date.year;
+	return out;
+}
--- LibraryManagement/Object/ItemDetail.h
+++ LibraryManagement/Object/ItemDetail.h
@@ -2,18 +2,8 @@
 
 #include <iostream>
 #include "Book.h"
+#include "../Container/Date.h"
 using namespace std;
-
-class Date
-{
-	private:
-		int day, month, year;
-
-	public:
-		Date(int day = 0, int month = 0, int year = 0);
-		Date(const Date& date);
-		friend ostream& operator<<(ostream& out, const Date& date);
-};
 
 class ItemDetail {
 	private:
@@ -29,20 +19,6 @@ class ItemDetail {
 
 		friend ostream& operator << (ostream& out, const ItemDetail& item);
 };
-
-/*=============Definition of Date============*/
-Date::Date(int day, int month, int year):
-	day(day), month(month), year(year) 
-{}
-
-Date::Date(const Date& date) :
-	day(date.day), month(date.month), year(date.year)
-{}
-
-ostream& operator << (ostream& out, const Date& date){
-	out << date.day << "/" << date.month << "/" << date.year;
-	return out;
-}
 
 /*=============Definition of ItemDetail===========*/
 
